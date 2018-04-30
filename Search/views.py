41d7a02aa26d558f8b27dfaa8f9daf3897fd8888
samir <abdallahsamir99@gmail.com>
2018-04-30 09:18:03 +0200

    el search gh ya wlad :D

--- Search/views.py
+++ Search/views.py
@@ -1,30 +1,42 @@
 from django.contrib.auth import authenticate
+from django.db import connection
 from django.shortcuts import render, redirect
 from Home.models import Person, Pet, HistoryRecord, PaymentMethod, PetCategory, PetCategoryKind
 from Search.models import RecentSearch, Comment
 
 
 def search(request):
+    cursor = connection.cursor()
     try:
-        user = Person.objects.get(username=request.user)
+        username = request.user.username
+        # user = Person.objects.get(username=username)
+        cursor.execute("SELECT id FROM auth_user WHERE username = %s", [username])
+        id_t = list(cursor.fetchone())[0]
+        user = list(Person.objects.raw("SELECT * FROM Home_person WHERE user_ptr_id = %s", [id_t]))[0]
     except:
         user = None
-    results = Pet.objects.all()
+
+    # results = Pet.objects.all()
+    results = Pet.objects.raw("SELECT * FROM Home_pet")
+    # print(results)
     try:
-        category = request.GET.__getitem__('category')
+        category = request.GET['category']
         if category != '':
-            results = results.filter(category__category=category)
+            # results = results.filter(category__category=category)
+            cursor.execute("SELECT id FROM Home_petcategory WHERE category = %s", [category])
+            c_id = list(cursor.fetchone())[0]
+            results = list(Pet.objects.raw("SELECT * FROM Home_pet WHERE category_id = %s", [c_id]))
     except:
         pass
 
     try:
-        s_age = int(request.GET.__getitem__('s_age'))
-        e_age = int(request.GET.__getitem__('e_age'))
+        s_age = int(request.GET['s_age'])
+        e_age = int(request.GET['e_age'])
         results = results.filter(age__range=[s_age, e_age]).order_by('age')
     except:
         pass
     if user is not None:
-        recent_search = RecentSearch(keys=' age from : ' + request.GET.__getitem__('s_age') + ' to : ' + request.GET.__getitem__('e_age') + ' category : ' + category, user=user)
+        recent_search = RecentSearch(keys=' age from : ' + request.GET['s_age'] + ' to : ' + request.GET['e_age'] + ' category : ' + category, user=user)
         recent_search.save()
     context = {
         'user': user,
@@ -34,12 +46,21 @@ def search(request):
 
 
 def details(request, pet_pk):
+    cursor = connection.cursor()
     try:
-        user = Person.objects.get(username=request.user)
+        username = request.user.username
+        # user = Person.objects.get(username=username)
+        cursor.execute("SELECT id FROM auth_user WHERE username = %s", [username])
+        id_t = list(cursor.fetchone())[0]
+        user = list(Person.objects.raw("SELECT * FROM Home_person WHERE user_ptr_id = %s", [id_t]))[0]
     except:
         user = None
-    pet = Pet.objects.get(pk=pet_pk)
-    records = HistoryRecord.objects.filter(pet=pet)
+
+    # pet = Pet.objects.get(pk=pet_pk)
+    pet = list(Pet.objects.raw("SELECT * FROM Home_pet WHERE id = %s", [pet_pk]))[0]
+    print(pet)
+    # records = HistoryRecord.objects.filter(pet=pet)
+    records = list(HistoryRecord.objects.raw("SELECT * FROM Home_historyrecord WHERE pet_id = %s", [pet_pk]))
     context = {
         'user': user,
         'pet': pet,
@@ -50,26 +71,35 @@ def details(request, pet_pk):
 
 
 def addcomment(request, pet_pk, comment_pk):
+    cursor = connection.cursor()
     if request.method == 'GET':
-        user = Person.objects.get(username=request.user)
+        username = request.user.username
+        cursor.execute("SELECT id FROM auth_user WHERE username = %s", [username])
+        id_t = list(cursor.fetchone())[0]
+        user = list(Person.objects.raw("SELECT * FROM Home_person WHERE user_ptr_id = %s", [id_t]))[0]
         if user.is_active:
-            pet = Pet.objects.get(pk=pet_pk)
-            body = request.GET.__getitem__('body')
+            pet = list(Pet.objects.raw("SELECT * FROM Home_pet WHERE id = %s", [pet_pk]))[0]
+            body = request.GET['body']
             comment = Comment(user=user, body=body, pet=pet)
             comment.save()
             return redirect('/search/details/' + str(pet_pk) + '/')
 
         return redirect('/search/details/' + str(pet_pk) + '/')
     else:
-        Comment.objects.get(pk=comment_pk).delete()
+        cursor.execute("DELETE FROM Search_comment WHERE id= %s",[comment_pk])
         return redirect('/search/details/' + str(pet_pk) + '/')
 
 
 
 def verify(request, pet_pk):
+    username = request.user.username
+    cursor = connection.cursor()
+    cursor.execute("SELECT id FROM auth_user WHERE username = %s", [username])
+    id_t = list(cursor.fetchone())[0]
+    user = list(Person.objects.raw("SELECT * FROM Home_person WHERE user_ptr_id = %s", [id_t]))[0]
+    pet = list(Pet.objects.raw("SELECT * FROM Home_pet WHERE id = %s", [pet_pk]))[0]
+
     if request.method == 'GET':
-        user = Person.objects.get(username=request.user)
-        pet = Pet.objects.get(pk=pet_pk)
         error = ''
         context = {
             'error': error,
@@ -79,11 +109,9 @@ def verify(request, pet_pk):
         return render(request, 'verify.html', context)
 
     else:
-        user = Person.objects.get(username=request.user)
-        pet = Pet.objects.get(pk=pet_pk)
-        type = request.POST.__getitem__('type')
-        number = request.POST.__getitem__('number')
-        password = request.POST.__getitem__('password')
+        type = request.POST['type']
+        number = request.POST['number']
+        password = request.POST['password']
         if PaymentMethod.objects.filter(user=user, number=number, type=type).exists() and pet.state == 'available':
             user = authenticate(username=request.user, password=password)
             if user is not None:
